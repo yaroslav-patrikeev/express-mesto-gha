d3b0e19b532d81a6c3c9d280723ab5f6bfc3fe16
Yaroslav Patrikeev <yaroslav.patrikeev@gmail.com>
2023-07-30 15:20:26 +0300

    fix 404 for /

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const cardsRouter = require('./cardsRouter');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
+router.use('/', (req, res) => {
+  res.status(404).send({ message: 'Not Found' });
+});
 
 module.exports = router;
