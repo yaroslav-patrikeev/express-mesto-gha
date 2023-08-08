bd4279c6be8103b4c69f7f04d749be5360e9ec02
Yaroslav Patrikeev <yaroslav.patrikeev@gmail.com>
2023-08-09 01:31:36 +0300

    fix 404 error throw

--- utils/sendResponse.js
+++ utils/sendResponse.js
@@ -7,11 +7,12 @@ const SUCCESSFUL_REQUEST = 200;
 const sendResponse = (promise, res, next) => {
   promise
     .then((data) => {
-      if (!data || data === null) throw new NotFoundError('Не найдено');
+      if (!data) throw new NotFoundError('Не найдено');
       return res.status(SUCCESSFUL_REQUEST).send(data);
     })
     .catch((err) => {
-      if (err.name === 'ValidationError' || err.name === 'CastError') throw new IncorrectDataError('Некорректные данные');
+      if (err.statusCode === 404) throw err;
+      if (['ValidationError', 'CastError'].includes(err.name)) throw new IncorrectDataError('Некорректные данные');
       throw new ServerError('Ошибка на сервере');
     })
     .catch(next);
