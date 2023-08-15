4a6f0f9be2dc1cc07f4dd0de01ed50e905cf914d
Yaroslav Patrikeev <yaroslav.patrikeev@gmail.com>
2023-08-15 13:59:11 +0300

    Pr14 fix-v5

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,13 +6,13 @@ const { JWT_SECRET = 'ab7b913c948c8378b9a3eac83d7b2d0c' } = process.env;
 const auth = (req, res, next) => {
   const { token } = req.cookies;
   if (!token) {
-    throw UnauthorizedError('Необходима авторизация');
+    throw new UnauthorizedError('Необходима авторизация');
   }
   let payload;
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    throw UnauthorizedError('Необходима авторизация');
+    throw new UnauthorizedError('Необходима авторизация');
   }
 
   req.user = payload;
